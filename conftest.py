b5be304b81d24de4cb444b8d0365bec6f0219634
Yuliya Savickaya <ysavickaya@marfa-tech.com>
2025-06-26 11:11:45 +0300

    add .env

--- conftest.py
+++ conftest.py
@@ -1,7 +1,20 @@
 import pytest
+import os
+
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 
 @pytest.fixture(scope='function')
@@ -18,8 +31,9 @@ def browser():
         "goog:loggingPrefs": {"browser": "ALL"}
     }
     options.capabilities.update(selenoid_capabilities)
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
 
